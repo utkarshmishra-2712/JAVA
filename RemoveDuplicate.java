da9b893da2b21ee93d0ba6408ef15c52b49e8bce
Utkarsh <utkarshwork2712@gmail.com>
2026-09-18 12:05:19 +0530

    Remove Duplicate

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -1,4 +1,30 @@
 package Basics;
 
+import java.util.*;
+
 public class RemoveDuplicate {
+
+    public static Set<String> removeDuplicates(List<String> names) {
+        Set<String> result = new HashSet<>();
+        result.addAll(names);
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+
+        names.add("Aman");
+        names.add("Riya");
+        names.add("Karan");
+        names.add("Aman");
+        names.add("Riya");
+        names.add("Simran");
+        names.add("Karan");
+
+        Set<String> uniqueNames = removeDuplicates(names);
+
+        System.out.println("Original List: " + names);
+        System.out.println("Unique Names: " + uniqueNames);
+    }
 }
